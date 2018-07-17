8c5b2dec3e37fa80d903d398895c7110a1f11844
pengmin3@163.com <pengmin3@163.com>
2018-07-17 10:49:59 +0800

    swagger2 启动配置

--- src/main/java/com/attendance/AttendanceSystemApplication.java
+++ src/main/java/com/attendance/AttendanceSystemApplication.java
@@ -3,6 +3,7 @@ package com.attendance;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @MapperScan("com.attendance.mapper")
